456e794542d2c516876f461779147a5104c35f75
vivi-PC\vivi <stefanpader@hotmail.com>
2017-06-24 17:03:52 +0200

    Revert "Revert "init""
    This reverts commit d5c04a00bbf541e83f5206e9382293c1674dd904.

---
+++ Assets/Scripts/MenuCtrl.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuCtrl : MonoBehaviour {
+
+	public void loadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+
+    public GameObject player;
+    private static SpriteRenderer faceDirection;
+    public float speed = 10.0F;
+    public float rotationSpeed = 100.0F;
+    void Update()
+    {
+        faceDirection = player.GetComponent<SpriteRenderer>();
+        float translation = Input.GetAxis("Horizontal") * speed;
+        translation *= Time.deltaTime;
+        transform.Translate(translation, 0, 0);
+        if(translation < 0)
+        {
+            faceDirection.flipX = false;
+        }
+        if(translation > 0)
+        {
+            faceDirection.flipX = true;
+        }
+
+    }
+}
